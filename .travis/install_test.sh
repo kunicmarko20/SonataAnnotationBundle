c2dd295bbd99219384be506dfb9af610dc8f5499
Marko Kunic <kunicmarko20@gmail.com>
2018-03-18 11:59:54 +0100

    start adding tests, add flintci options, fix travisci test script

--- .travis/install_test.sh
+++ .travis/install_test.sh
@@ -3,6 +3,10 @@ set -ev
 
 mkdir --parents "${HOME}/bin"
 
+# PHPUnit install
+wget "https://phar.phpunit.de/phpunit-7.0.phar" --output-document="${HOME}/bin/phpunit"
+chmod u+x "${HOME}/bin/phpunit"
+
 # Coveralls client install
 wget https://github.com/satooshi/php-coveralls/releases/download/v1.0.1/coveralls.phar --output-document="${HOME}/bin/coveralls"
 chmod u+x "${HOME}/bin/coveralls"
---
+++ tests/SonataAnnotationBundleTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace KunicMarko\FormAnnotationBundle\Tests;
+
+use KunicMarko\SonataAnnotationBundle\DependencyInjection\Compiler\AutoRegisterPass;
+use KunicMarko\SonataAnnotationBundle\SonataAnnotationBundle;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Marko Kunic <kunicmarko20@gmail.com>
+ */
+class SonataAnnotationBundleTest extends TestCase
+{
+    /**
+     * @var SonataAnnotationBundle
+     */
+    private $bundle;
+
+    protected function setUp()
+    {
+        $this->bundle = new SonataAnnotationBundle();
+    }
+
+    public function testBundle()
+    {
+        $this->assertInstanceOf(Bundle::class, $this->bundle);
+    }
+
+    public function testCompilerPasses()
+    {
+        $containerBuilder = $this->prophesize(ContainerBuilder::class);
+
+        $containerBuilder->addCompilerPass(
+            Argument::type(AutoRegisterPass::class),
+            Argument::cetera()
+        )->shouldBeCalledTimes(1);
+
+        $this->bundle->build($containerBuilder->reveal());
+    }
+}
