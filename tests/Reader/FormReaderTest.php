cff80d3cfd24ed3b8c48bc08eb598d1ee983cd90
Marko Kunic <kunicmarko20@gmail.com>
2019-05-16 21:15:45 +0100

    Add position to form fields (#32)

--- tests/Reader/FormReaderTest.php
+++ tests/Reader/FormReaderTest.php
@@ -7,6 +7,7 @@ namespace KunicMarko\SonataAnnotationBundle\Tests\Reader;
 use Doctrine\Common\Annotations\AnnotationReader;
 use KunicMarko\SonataAnnotationBundle\Reader\FormReader;
 use KunicMarko\SonataAnnotationBundle\Tests\Fixtures\AnnotationClass;
+use KunicMarko\SonataAnnotationBundle\Tests\Fixtures\AnnotationExceptionClass;
 use KunicMarko\SonataAnnotationBundle\Tests\Fixtures\EmptyClass;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Argument;
@@ -42,6 +43,7 @@ final class FormReaderTest extends TestCase
     {
         $this->formMapper->add('field', Argument::cetera())->shouldBeCalled();
         $this->formMapper->add('parent', Argument::cetera())->shouldBeCalled();
+        $this->formMapper->add('additionalField2', Argument::cetera())->shouldBeCalled();
 
         $this->formReader->configureCreateFields(
             new \ReflectionClass(AnnotationClass::class),
@@ -53,9 +55,41 @@ final class FormReaderTest extends TestCase
     {
         $this->formMapper->add('additionalField', Argument::cetera())->shouldBeCalled();
         $this->formMapper->add('parent', Argument::cetera())->shouldBeCalled();
+        $this->formMapper->add('additionalField2', Argument::cetera())->shouldBeCalled();
 
         $this->formReader->configureEditFields(
             new \ReflectionClass(AnnotationClass::class),
+            $this->formMapper->reveal()
+        );
+    }
+
+    public function testConfigureCreateFieldsAnnotationPresentPosition(): void
+    {
+        $mock = $this->createMock(FormMapper::class);
+
+        $properties = ['parent', 'field', 'additionalField2'];
+        $mock->expects($this->exactly(3))
+            ->method('add')
+            ->with($this->callback(static function(string $field) use (&$properties): bool {
+                $property = array_shift($properties);
+                return $field === $property;
+            }));
+
+        $this->formReader->configureCreateFields(
+            new \ReflectionClass(AnnotationClass::class),
+            $mock
+        );
+    }
+
+    /**
+     * @group legacy
+     */
+    public function testPositionShouldBeUnique(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Position "1" is already in use by "field", try setting a different position for "field2".');
+        $this->formReader->configureCreateFields(
+            new \ReflectionClass(AnnotationExceptionClass::class),
             $this->formMapper->reveal()
         );
     }
